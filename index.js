6d088a1cea10e925e5ddd070e29d5fd03d513c29
Rian Wouters <rian.wouters@lighting.com>
2018-06-08 14:32:49 +0200

    add function

---
+++ index.js
@@ -0,0 +1,19 @@
+const interpolations = {
+    "Fn::RandomFloat": ({min, range}) => Math.random() * range + min,
+    "Fn::Moment": ({format}) => moment().format(format),
+    "Fn::ConvertType": ({type, value}) => global[type](value),
+    "Fn::Eval": s => eval(s),
+    get: name => name.startsWith('Fn::') ? interpolations[name] : undefined
+};
+
+const interpolate = (json, depth) => {
+    if (typeof json === 'object')
+        for (const key in json) {
+            if (!depth || depth > 0) json[key] = interpolate(json[key], depth - 1);
+            const ip = interpolations.get(key);
+            if (ip) return ip(json[key]);
+        }
+    return json;
+};
+
+module.exports = interpolate;
